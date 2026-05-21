e7af24a378d2c17ef34ee27a215bad8e4678c284
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-05-21 09:41:24 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,10 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://rvgcniaowzmsudzliozf.supabase.co";
+  "https://vhzuisrsrnxofuxyymfa.supabase.co/rest/v1/";
 
 const supabaseAnonKey =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2Z2NuaWFvd3ptc3Vkemxpb3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjQxNzQsImV4cCI6MjA5MTQwMDE3NH0.uwwKFLuK-XyPoXPrB6_CseRTiD9d-iyMQSPWrFw-l-I";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoenVpc3Jzcm54b2Z1eHl5bWZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMDQ4NzYsImV4cCI6MjA5NDg4MDg3Nn0.x49X7CGO0Qill0a6wZ1HDQIZY4rjkzt60J1lzGfxj1o";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
